0dff2edc7b490a1efc6996bd1cf590b265959c7f
veraaaaa <wang_yuwei_vera@126.com>
2018-05-28 06:31:07 +1000

    Add files via upload
    week 5 Table and collection views(show a list of trip destination in a table view）

---
+++ tripApp/tripApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tripApp
+//
+//  Created by 王雨薇 on 2018/5/24.
+//  Copyright © 2018年 wangyuwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
